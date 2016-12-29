510a56f1dce95b60e292f6911959065e30773380
Ryan Kayu Ching <ryanching@hotmail.com>
2016-12-29 15:31:32 -0800

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ var express = require("express"); //This connects the express module API
 
 //Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.headers["x-forwarded-proto"] === "http"){
+    next();
+  } else {
+    res.redirect("http://" + req.hostname + req.url);
+  }
+});
 
 app.use(express.static("public"));
 //Add functionality to your app
 //express.static identifies what folder it wants to serve
 
-app.listen(3000, function(){
-  console.log("Express server is up on port 3000");
+app.listen(PORT, function(){
+  console.log("Express server is up on port " + PORT);
 });
